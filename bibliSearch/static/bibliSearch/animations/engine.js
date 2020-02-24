33f0e0deb10e373b5efed1ed93e0edf1d30d40e7
axelzzz <axelzavier@gmail.com>
2020-02-24 23:35:05 +0100

    ajout db + test py4j + test jsonResponse

---
+++ bibliSearch/static/bibliSearch/animations/engine.js
@@ -0,0 +1,6 @@
+document.getElementById('form_search').onsubmit = updateView;
+
+function updateView() {
+  console.log('aaaaaaaa');
+}
+
--- bibliSearch/views.py
+++ bibliSearch/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 
 def index(request):
     return render(request, 'bibliSearch/index.html')
 
 
 def searchByTitle(request):
-    return HttpResponse("you searched %s" %request.GET.get('book_title', ' hum '))
+    #return render(request, 'bibliSearch/index.html')
+    #return HttpResponse("you searched %s" %request.GET.get('book_title', ' hum '))
+    json_books = {"books" : [request.GET.get('book_title'), "bible"]}
+    return JsonResponse(json_books)
+    #return HttpResponse(json.dumps(json_books), content_type="application/json")
